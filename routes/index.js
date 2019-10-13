e51837b085229458d5038adac5bbe2230dd86ac5
Javi Gomez <rtomyj@gmail.com>
2019-10-13 00:55:06 -0500

    Deleting defaults

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+let router = require('express').Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('welcome');
 });
 
 module.exports = router;
