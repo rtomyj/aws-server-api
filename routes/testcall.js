b33a4e654cab6b8cecefad2834230605aa6a5cfe
Javi Gomez <rtomyj@gmail.com>
2019-10-15 19:36:08 -0500

    Repurposed / endpoint to /testcall

--- routes/testcall.js
+++ routes/testcall.js
@@ -1,8 +1,9 @@
 let router = require('express').Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('{welcome}')
+router.get('/', function(req, res, next)
+{
+  res.send('api up and running')
 })
 
 module.exports = router
