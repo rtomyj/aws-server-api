bd16db9297600a9e214435359185d4963b6bd0e1
Javi Gomez <rtomyj@gmail.com>
2020-01-08 23:13:26 -0600

    Updated cred manager to clear creds on demand

--- routes/s3/config.js
+++ routes/s3/config.js
@@ -1,20 +1,30 @@
 
-let AWS = require('aws-sdk')
+const AWS = require('aws-sdk')
 
-function createS3Instance(headers, params)
+const createS3Instance = (creds, s3Params) =>
 {
-	let { accesskeyid, secretaccesskey } = headers
-	console.log(headers)
+	const { accessKeyId, secretAccessKey } = creds
+	const s3BucketEndpoint = (creds.s3BucketEndpoint === "" || creds.s3BucketEndpoint === undefined) ? false : creds.s3BucketEndpoint
+	AWS.config.update( {
+		credentials: new AWS.Credentials(accessKeyId, secretAccessKey),
+		s3BucketEndpoint: s3BucketEndpoint
+	} )
+	//console.log(AWS.config)
 
-	let config =
-	{
-		'accessKeyId': accesskeyid
-		, 'secretAccessKey': secretaccesskey
-	}
-	AWS.config.update(config)
+	return new AWS.S3({ maxRetries: 1, params: s3Params })
+}
 
-	return new AWS.S3({ maxRetries: 1, params: params })
+const cleanup = () => {
+	AWS.config.update( {
+		credentials: new AWS.Credentials( undefined, undefined ),
+		s3BucketEndpoint: false
+	} )
+
+	console.log(AWS.config)
 }
 
 
-module.exports = createS3Instance
+module.exports = {
+	createS3Instance,
+	cleanup
+}
